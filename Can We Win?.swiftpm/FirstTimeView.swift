6137cd625c7eeb0818dbb311d2e01e0faf073c63
potato dog man <saumil707@gmail.com>
2023-04-12 22:37:06 +0800

    Disbanded (for now)
    issues
    1)login page does not save can
    2)design is quite lacking
    3)login page has weird stretching
    
    --end--

--- Can We Win?.swiftpm/FirstTimeView.swift
+++ Can We Win?.swiftpm/FirstTimeView.swift
@@ -7,12 +7,27 @@
 
 import SwiftUI
 
+enum ActionState: Int {
+    case setup = 0
+    case readyForPush = 1
+}
+
+
 @available(iOS 16.0, *)
 
 struct FirstTimeView: View {
+    
+    
+    
+
+    
+    
+    
     @State var name: String = ""
     @State var price: String = ""
     @State var location: String = ""
+    
+    @State private var actionState: ActionState? = .setup
     
     @Binding var plants: [Drink]
     
@@ -39,33 +54,40 @@ struct FirstTimeView: View {
                     
                     
                     
-//                    Button("Save") {
-//                        let plant = Drink(
-//                            name: name,
-//                            price: price,
-//                            location: location)
-//                        plants.append(plant)
-//
-                    
-                    
-                    NavigationLink(destination: IhaveappView()) {
+                    Button("Save") {
+                        let plant = Drink(
+                            name: name,
+                            price: price,
+                            location: location)
+                        plants.append(plant)
                         
-                        Text("Save")
                         
-//                        let plant = Drink(
-//                            name: name,
-//                            price: price,
-//                            location: location)
-//                        plants.append(plant)
-//                        
+                        
+                        
+                        //                    NavigationLink(destination: IhaveappView()){
+                        //
+                        //                        Text("Save")
+                        //
+                        ////                        let plant = Drink(
+                        ////                            name: name,
+                        ////                            price: price,
+                        ////                            location: location)
+                        ////                        plants.append(plant)
+                        ////
+                        //                        self.asyncTask()
+                        //
+                        //                    }
+                        
                     }
-                    
                 }
             }
+            .navigationTitle("Add Custom Can")
         }
-        .navigationTitle("Add Custom Can")
+        
+        
+        
+        
+        
+        
     }
 }
-
-
-
